6a3647901c3397d47590ea03a92359d381bc73f0
Akash Mahadik <akashpm1996@gmail.com>
2023-12-30 15:19:10 +0530

    Initial commit

---
+++ app.vue
@@ -0,0 +1,194 @@
+<template>
+  <div class="relative bg-black text-white">
+    <!-- <header class="w-full bg-white">
+      <nav class="container"></nav>
+    </header> -->
+
+    <main>
+      <section class="">
+        <div class="container relative max-w-screen-xl mx-auto">
+          <img src="/images/MadoneSLR7_23_37025_A_Portrait.jpg" alt="" class="object-cover w-11/12 h-auto ms-auto">
+          <div class="flex flex-col h-full justify-center absolute inset-0">
+            <div class="pb-24">
+              <p class="text-sm font-semibold uppercase tracking-widest underline underline-offset-4 mb-2">The ultimate
+                race
+                bike</p>
+              <h1 class="text-6xl font-bold uppercase">Madone SLR 7</h1>
+              <h3 class="text-4xl uppercase opacity-70">Gen 7</h3>
+              <p class="text-base font-semibold my-3">₹675,599</p>
+              <p class="text-base font-light tracking-wider max-w-[45ch] opacity-70">The Madone SLR 7 is the ultimate race
+                machine. An 800 Series OCLV
+                Carbon frame with exclusive IsoFlow technology adds an aerodynamic advantage, cuts weight and smooths the
+                road ahead. It's
+                built up with the ultra-fast precision shifting of Shimano's wireless electronic Ultegra Di2 drivetrain
+                and
+                deep carbon wheels that cut through the wind.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="">
+        <div class="container relative max-w-screen-xl mx-auto py-20">
+          <div class="absolute top-0 right-0 translate-x-1/2">
+            <p
+              class="text-[200px] leading-none font-bold uppercase text-center bg-clip-text text-transparent bg-gradient-to-b from-[#373737] via-[#373737]/40 via-40%">
+              Madone</p>
+          </div>
+          <div class="absolute left-0 bottom-[-110%] -translate-x-2/3">
+            <p
+              class="text-[200px] leading-none font-bold uppercase text-center bg-clip-text text-transparent bg-gradient-to-b from-[#373737] via-[#373737]/40 via-40%">
+              SLR 7</p>
+          </div>
+          <div class="absolute bottom-[-210%] right-0 translate-x-2/3">
+            <p
+              class="text-[200px] leading-none font-bold uppercase text-center bg-clip-text text-transparent bg-gradient-to-b from-[#373737] via-[#373737]/40 via-40%">
+              Gen 7</p>
+          </div>
+          <div class="flex items-center gap-8">
+            <div class="flex-1 flex flex-col justify-center pr-8">
+              <div class="me-auto w-max">
+                <p class="text-8xl font-bold mb-8">-300g</p>
+                <p class="text-2xl font-semibold">Nearly 300 grams <span class="font-extralight">lighter*</span></p>
+              </div>
+              <div class="ms-auto w-max">
+                <p class="text-8xl font-bold mb-8">+60s</p>
+                <p class="text-2xl font-semibold">60 seconds/hour <span class="font-light italic">faster*</span></p>
+              </div>
+            </div>
+            <div class="flex-1">
+              <div class="mb-8">
+                <h1 class="text-5xl font-bold uppercase">A new standard of speed</h1>
+                <h3 class="text-4xl font-light opacity-70">Now More Lighter. More Faster.</h3>
+              </div>
+              <p>We set out to make Madone the fastest road bike in the world. Years of research and development
+                informed every aerodynamic inch of the seventh-generation Madone SLR to create a frame, cockpit and
+                rider position that’s a whopping 19 watts faster than ever before.*
+                <span class="font-semibold">*Compared to previous generation at 45 km/h</span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="">
+        <div class="container relative max-w-screen-xl mx-auto py-20">
+          <div class="flex flex-row-reverse items-center gap-8">
+            <div class="flex-1">
+          <img src="/images/MadoneSLR7_23_37025_A_Alt3.webp" alt="" class="object-cover h-auto ms-auto">
+            </div>
+            <div class="flex-1">
+              <div class="mb-8">
+                <h1 class="text-5xl font-bold uppercase">Never-before-seen IsoFlow Technology</h1>
+                <h3 class="text-4xl font-light opacity-70">Brutal Performance. Total Comfort.</h3>
+              </div>
+              <p>All-new IsoFlow technology soaks up fatiguing bumps in the road so you can ride stronger for longer. A
+                race-focused evolution of IsoSpeed comfort, IsoFlow flexes over bumps for a smoother ride, improves
+                aerodynamics and shaves weight to save precious seconds in the saddle.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="">
+        <div class="container relative max-w-screen-xl mx-auto py-20">
+          <div class="flex items-center gap-8">
+            <div class="flex-1">
+
+            </div>
+            <div class="flex-1">
+              <div class="mb-8">
+                <h1 class="text-5xl font-bold uppercase">Unprecedented road bike aerodynamics</h1>
+                <h3 class="text-4xl font-light opacity-70">Advances in Aerodynamic Design</h3>
+              </div>
+              <p>The devil is in the details when it comes to heart-pounding, race-winning speed. Every element of the
+                new Madone Gen 7 was expertly sculpted to create our most aerodynamic road race bike ever, including a
+                new generation of Kammtail tube shapes, an aero-optimised cockpit and all-new IsoFlow technology that
+                smooths air as it moves over the bike and accelerates it through the frame for even more free speed.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="">
+        <div class="container relative max-w-screen-xl mx-auto py-20">
+          <div class="flex flex-row-reverse items-center gap-8">
+            <div class="flex-1">
+
+            </div>
+            <div class="flex-1">
+              <div class="mb-8">
+                <h1 class="text-5xl font-bold uppercase">Positioned for power</h1>
+                <h3 class="text-4xl font-light opacity-70">Ergonomic and Aerodynamic Riding Position</h3>
+              </div>
+              <p>An overhauled bar/stem with a shorter reach and hoods that are 3 cm narrower than the drops move you
+                into a more comfortable ergonomic riding position. This narrower hood position also greatly reduces
+                rider drag for extra aerodynamic speed while maintaining control in the drops so you don’t lose power in
+                explosive moments.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="">
+        <div class="container relative max-w-screen-xl mx-auto py-20">
+          <div class="flex items-center gap-8">
+            <div class="flex-1">
+
+            </div>
+            <div class="flex-1">
+              <div class="mb-8">
+                <h1 class="text-5xl font-bold uppercase">Lighter than ever</h1>
+                <h3 class="text-4xl font-light opacity-70">New Gram-Shaving Design</h3>
+              </div>
+              <p>A new lightweight design and our best and lightest OCLV Carbon make the seventh-generation Madone our
+                lightest Madone disc ever, shaving around 300 grams off the previous ultra-lightweight version.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!-- 
+      <section class="min-h-screen">
+        <div class="container relative max-w-screen-xl mx-auto py-16">
+          <p
+            class="text-6xl font-bold uppercase text-center bg-clip-text text-transparent bg-gradient-to-b from-[#373737] via-[#373737]/40 via-40%">
+            Features</p>
+        </div>
+      </section>
+
+      <section class="min-h-screen">
+        <div class="container relative max-w-screen-xl mx-auto py-16">
+          <p
+            class="text-6xl font-bold uppercase text-center bg-clip-text text-transparent bg-gradient-to-b from-[#373737] via-[#373737]/40 via-40%">
+            Specifications</p>
+        </div>
+      </section>
+
+      <section class="min-h-screen">
+        <div class="container relative max-w-screen-xl mx-auto py-16">
+          <p
+            class="text-6xl font-bold uppercase text-center bg-clip-text text-transparent bg-gradient-to-b from-[#373737] via-[#373737]/40 via-40%">
+            Sizing</p>
+        </div>
+      </section>
+
+      <section class="min-h-screen">
+        <div class="container relative max-w-screen-xl mx-auto py-16">
+          <p
+            class="text-6xl font-bold uppercase text-center bg-clip-text text-transparent bg-gradient-to-b from-[#373737] via-[#373737]/40 via-40%">
+            Geometry</p>
+        </div>
+      </section> 
+    -->
+    </main>
+
+    <footer>
+
+    </footer>
+  </div>
+</template>
